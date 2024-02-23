40df41ef025501d6795bb4bbab8febedb92e127d
Tavo Cruz <madecruz2@gmail.com>
2024-02-22 22:11:20 -0800

    add react-router-dom

--- realtimechatapp-client/src/App.jsx
+++ realtimechatapp-client/src/App.jsx
@@ -1,15 +1,18 @@
 import Home from "./pages/home/Home";
 import './App.css'
-// import Login from './pages/login/Login'
-// import SignUp from './pages/signup/SignUp'
+import { Route, Routes } from "react-router-dom";
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Login /> */}
-      {/* <SignUp /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   )
 }
