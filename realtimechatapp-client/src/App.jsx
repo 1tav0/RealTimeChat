2326d876936fb2a7d8e2ed65d03faaf349fced11
Tavo Cruz <madecruz2@gmail.com>
2024-02-23 12:20:22 -0800

    more useContext

--- realtimechatapp-client/src/App.jsx
+++ realtimechatapp-client/src/App.jsx
@@ -1,18 +1,20 @@
 import Home from "./pages/home/Home";
 import './App.css'
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-
+  const { authUser } = useAuthContext();
+  const navigate = useNavigate();
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={authUser ? navigate("/") : <SignUp />} />
       </Routes>
       <Toaster/>
     </div>
