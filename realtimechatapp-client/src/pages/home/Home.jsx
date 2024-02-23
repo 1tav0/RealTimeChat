47dd76d3df543cce866d26eaeb1bf53071cb8c79
Tavo Cruz <madecruz2@gmail.com>
2024-02-22 18:35:22 -0800

    Front end components

--- realtimechatapp-client/src/pages/home/Home.jsx
+++ realtimechatapp-client/src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
+import Sidebar from "../../components/Sidebar/Sidebar"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      {/* <MessagesContainer /> */}
+    </div>
   )
 }
 
