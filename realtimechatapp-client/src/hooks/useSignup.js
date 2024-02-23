4c40450e03ff4f3a9467675b5bbf307e7f3a955e
Tavo Cruz <madecruz2@gmail.com>
2024-02-23 10:32:08 -0800

    adding signup and axios functionality

---
+++ realtimechatapp-client/src/hooks/useSignup.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import api from "../api/axiosConfig";
+// custom hook to make it cleaner in the signup component with less code 
+import toast from "react-hot-toast";
+
+// will return a state 
+const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+
+  const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
+    const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
+    if (!success) {
+      return;
+    }
+    setLoading(true);
+    try {
+      // const response = await fetch("http://localhost:5000/api/auth/signup", {
+      //   method: "Post",
+      //   headers: {
+      //     "Content-Type": "application/json"
+      //   },
+      //   body: JSON.stringify({
+      //     fullName,
+      //     username,
+      //     password,
+      //     confirmPassword,
+      //     gender
+      //   })
+      // })
+      const response = await api.post("/api/v1/auth/signup", {
+        fullName,
+        username,
+        password,
+        confirmPassword,
+        gender
+      })
+      const data = await response.json();
+      console.log(data);
+
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+  return { loading, signup };
+}
+
+export default useSignup
+
+function handleInputErrors({ fullName, username, password, confirmPassword, gender }) {
+  if (!fullName || !username || !password || !confirmPassword || gender) {
+    toast.error('Please fill in all fields');
+    return false;
+  }
+  if (password !== confirmPassword) {
+    toast.error('Passwords do not match');
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error('Password length must be atleast 6 characters long');
+    return false;
+  }
+  return true;
+}
--- realtimechatapp-client/src/pages/signup/SignUp.jsx
+++ realtimechatapp-client/src/pages/signup/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 import { useState } from "react";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
 
@@ -17,9 +18,11 @@ const SignUp = () => {
     setInputs({...inputs, gender})
   }
 
-  const handleSubmit = (e) => {
+  const { loading, signup } = useSignup();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(inputs);
+    await signup(inputs);
   }
 
   return (
