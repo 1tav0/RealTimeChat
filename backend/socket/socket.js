4c50d58f0f5c8b5feb522addc198b6f5249df7c6
Tavo Cruz <madecruz2@gmail.com>
2024-02-27 15:16:30 -0800

    continue front end socket connection

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -14,12 +14,22 @@ const io = new Server(server, {
   }
 });
 
+// create a map of the sockets that are connected for that userid
+const userSocketMap = {}; // {userid: socketId}
+
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
   // socket.on() is used to listen to the events. can be used both on the client and on server side
   socket.on("disconnect", () => {
-    console.log("User disconnected")
+    console.log("User disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   })
 })
 
--- realtimechatapp-client/src/context/SocketContext.jsx
+++ realtimechatapp-client/src/context/SocketContext.jsx
@@ -15,9 +15,20 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000");
+      const socket = io("http://localhost:5000", {
+        query: {
+          userId: authUser._id
+        }
+      });
 
       setSocket(socket);
+
+      //socket.on() is used to listen to the events. can be used both on client and server side
+      // socket.emit() is used to send the events. can be used both on client and server side
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      })
+
       //clean up function
       return () => socket.close();
     } else {
