7a1842128f01fb13d9de5c9eb069c02712f05941
Tavo Cruz <madecruz2@gmail.com>
2024-02-27 12:11:16 -0800

    search functionality

--- realtimechatapp-client/src/components/Sidebar/SearchInput.jsx
+++ realtimechatapp-client/src/components/Sidebar/SearchInput.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search term must be at least 3 chars long");
+    }
+
+    const conversation = conversations.find((c) => {
+      return c.fullName.toLowerCase().includes(search.toLowerCase())
+    })
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error("No such user found!");
+    }
+  }
+
   return (
-    <form action="">
+    <form
+      className="flex items-center gap-2"
+      onSubmit={handleSubmit}
+    >
       <input type="text"
         placeholder="Search..."
         className="input input-bordered rounded-full"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
       />
       <button
         type="submit"
