137660291e052090cfe0b36d0de20c3234297d0f
Tavo Cruz <madecruz2@gmail.com>
2024-02-27 11:49:39 -0800

    add skeleton page when loading and make more dynamic

--- realtimechatapp-client/src/components/MessagesContainer/Message.jsx
+++ realtimechatapp-client/src/components/MessagesContainer/Message.jsx
@@ -1,35 +1,37 @@
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
+const Message = ({ message }) => {
+  //check if message is ours or from user
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  // to style the chat bubble direction
+  const chatClassname = fromMe ? "chat-end" : "chat-start";
+  // to choose profile pic
+  const profilePic = fromMe ? authUser.avatar : selectedConversation?.avatar;
+  // choose who sent messge
+  const name = fromMe ? authUser.fullName : selectedConversation.fullName;
+  // the background color
+  const backColor = fromMe ? "bg-blue-500" : "";
+  // make the time dynamic
+  const formattedDate = extractTime(message.createdAt);
   return (
     <div className="flex flex-col">
-      <div className="chat chat-end">
+      <div className={`chat ${chatClassname}`}>
         <div className="chat-header">
-          Obi-Wan Kenobi
+          {name}
         </div>
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
-            <img src="/cr7.jpg" alt="avatar" />
+            <img src={profilePic} alt="avatar" />
           </div>
         </div>
-        <div className="chat-bubble text-white">
-          Hi, wassup!
+        <div className={`chat-bubble text-white ${backColor}`}>
+          {message.message}
         </div>
-        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
-      </div>
-
-      <div className="chat chat-start">
-        <div className="chat-header">
-          Obi-Wan Kenobi
-        </div>
-        <div className="chat-image avatar">
-          <div className="w-10 rounded-full">
-            <img src="/cr7.jpg" alt="avatar" />
-          </div>
-        </div>
-        <div className="chat-bubble text-white">
-          Hi, wassup!
-        </div>
-        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedDate}</div>
       </div>
     </div>
   )
--- realtimechatapp-client/src/components/MessagesContainer/Messages.jsx
+++ realtimechatapp-client/src/components/MessagesContainer/Messages.jsx
@@ -1,13 +1,29 @@
 import useGetMessages from "../../hooks/useGetMessages"
-import Message from "./Message"
-
+import MessageSkeleton from "../Skeletons/MessageSkeleton";
+import Message from "./Message";
 const Messages = () => {
   const { loading, messages } = useGetMessages();
   console.log(messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
       {
-        loading ? 
+        // if is loading use the skeleton component 3 times
+        loading && [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)
+      }
+
+      {
+        !loading && messages.length > 0 && messages.map(message => (
+          <Message
+            key={message._id}
+            message={message}
+          />
+        ))
+      }
+
+      {
+        !loading && messages.length === 0 && (
+          <p className="text-center">Send a message to start the conversation</p>
+        )
       }
     </div>
   )
