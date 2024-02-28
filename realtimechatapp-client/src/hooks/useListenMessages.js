b56bd47324841eecd4b1321cfe8f437f8551e9b1
Tavo Cruz <madecruz2@gmail.com>
2024-02-27 16:31:18 -0800

    add notification sound

--- realtimechatapp-client/src/hooks/useListenMessages.js
+++ realtimechatapp-client/src/hooks/useListenMessages.js
@@ -1,17 +1,22 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext"
 import useConversation from "../zustand/useConversation";
+import notificationSound from "../assets/sounds/siri.mp3";
 
 const useListenMessages = () => {
-  const { socket, onlineUsers } = useSocketContext();
+  const { socket } = useSocketContext();
   const { messages, setMessages } = useConversation();
 
+  
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
       newMessage.shouldShake = true;
+      const sound = new Audio(notificationSound);
+      sound.play();
       setMessages([...messages, newMessage])
     });
 
+    // to not listen to an event more than once
     return () => socket?.off("newMessage");
   },[socket, setMessages, messages])
 }
